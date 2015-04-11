b7fb088bd223f3175bf38026135f6c19b6bbc451
Florian Hassanen <florian.hassanen@gmail.com>
2015-04-11 17:40:49 +0200

    ose color-theme mode due to emacs live

--- config/solarized-theme-conf.el
+++ config/solarized-theme-conf.el
@@ -1,3 +1,6 @@
-(add-to-list 'custom-theme-load-path (concat (live-pack-lib-dir) "emacs-color-theme-solarized"))
-
-(load-theme 'solarized t)
+; not compatible with emacs live :(
+;(add-to-list 'custom-theme-load-path (concat (live-pack-lib-dir) "emacs-color-theme-solarized"))
+;(load-theme 'solarized t)
+(live-add-pack-lib "emacs-color-theme-solarized")
+(require 'color-theme-solarized)
+(color-theme-solarized)
